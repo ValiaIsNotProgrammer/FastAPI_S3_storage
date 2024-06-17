1da63266438904b1ca25f1282907291dc9627d78
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-06-17 12:00:09 +0300

    create base model

---
+++ backend/photos/database/models/base.py
@@ -0,0 +1,14 @@
+import uuid
+from typing import Optional
+from uuid import UUID, uuid4
+
+from sqlalchemy.orm import declared_attr
+from sqlmodel import SQLModel, Field
+
+
+class ModelBase(SQLModel):
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    id: Optional[UUID] = Field(primary_key=True, index=True, default=uuid.uuid4(), schema_extra={"json_schema_extra": str(uuid.uuid4())})
