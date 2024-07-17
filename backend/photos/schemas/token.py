b3060f47f67f6526af1559bc0a4f602c0a8eb219
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-07-17 09:54:56 +0300

    create token schema

---
+++ backend/photos/schemas/token.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    sub: UUID
+    expire: datetime = None
+
