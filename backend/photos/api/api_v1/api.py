0d5a5d5182bad467e187d1a5e70da7a849bcc7ce
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-07-17 09:50:17 +0300

    update routers

--- backend/photos/api/api_v1/api.py
+++ backend/photos/api/api_v1/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from api.api_v1.endpoints.photos import router as photos_router
+from api.api_v1.endpoints.auth import router as auth_router
 
 
 api_router = APIRouter()
 
 api_router.include_router(photos_router, prefix="/photos", tags=["photos"])
+api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
